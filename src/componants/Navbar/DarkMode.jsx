766fb51309cf7feb1f8ff44647e71fc85bd6b8d3
Rahmatullah1983 <bugtirahmatullah@gmail.com>
2024-06-10 15:29:52 +0500

    made new changes in darkMode componant

--- src/componants/Navbar/DarkMode.jsx
+++ src/componants/Navbar/DarkMode.jsx
@@ -1,43 +1,28 @@
-
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import LightButton from "../../assets/website/light-mode-button.png";
 import DarkButton from "../../assets/website/dark-mode-button.png";
+
 const DarkMode = () => {
-    const [theme, setTheme] = React.useState(
-        localStorage.getItem("theme") ? localStorage.getItem
-            ("theme") : "light"
-    )
-    React.useEffect(() => {
+    const [theme, setTheme] = useState(localStorage.getItem("theme") || "light");
+
+    useEffect(() => {
         const element = document.documentElement;
-
-        if (theme === "dark") {
-            element.classList.add("dark");
-            localStorage.setItem("theme", "dark");
-        } else {
-            element.classList.remove("dark");
-            localStorage.setItem("theme", "light");
-        }
+        const isDark = theme === "dark";
+        element.classList.toggle("dark", isDark);
+        localStorage.setItem("theme", theme);
     }, [theme]);
-    const toggleTheme = () => {
-        setTheme(prevTheme => (prevTheme === "light" ? "dark" : "light"));
-    };
 
+    const toggleTheme = () => setTheme(prevTheme => (prevTheme === "light" ? "dark" : "light"));
 
     return (
+        <img
+            src={theme === "light" ? LightButton : DarkButton}
+            alt={`Switch to ${theme === "light" ? "Dark" : "Light"} Mode`}
+            onClick={toggleTheme}
+            className="w-12 cursor-pointer drop-shadow-[1px_1px_1px_rgba(0,0,0,0.9)] transition-all duration-300 absolute right-0 top-3 z-10"
+        />
+    );
+};
 
-        <div>
+export default DarkMode;
 
-            <img
-                src={theme === "light" ? LightButton : DarkButton}
-                alt={theme === "light" ? "Switch to Dark Mode" : "Switch to Light Mode"}
-                onClick={toggleTheme}
-                className={`w-12 cursor-pointer drop-shadow-[1px_1px_1px_rgba(0,0,0,0.9)] transition-all duration-300 absolute right-0 top-3 z-10`}
-            />
-        </div>
-
-
-
-    )
-}
-
-export default DarkMode
